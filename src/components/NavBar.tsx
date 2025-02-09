554ca56af7213be4b2ec515184292cb242bbf40d
jolene <169898727+jolenetan1234@users.noreply.github.com>
2025-02-10 01:58:10 +0800

    fix build issues

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,61 +1,8 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import { DrawOutlineButton, HamburgerButton } from "./ui/buttons";
-import { Separator } from "./ui/separator";
-
-// import * as React from "react"
-// import Link from "next/link"
-
-// import { cn } from "@/lib/utils"
-// // import { Icons } from "@/components/icons"
-// import {
-//   NavigationMenu,
-//   NavigationMenuContent,
-//   NavigationMenuItem,
-//   NavigationMenuLink,
-//   NavigationMenuList,
-//   NavigationMenuTrigger,
-//   navigationMenuTriggerStyle,
-// } from "@/components/ui/navigation-menu"
-
-// const components: { title: string; href: string; description: string }[] = [
-//   {
-//     title: "Alert Dialog",
-//     href: "/docs/primitives/alert-dialog",
-//     description:
-//       "A modal dialog that interrupts the user with important content and expects a response.",
-//   },
-//   {
-//     title: "Hover Card",
-//     href: "/docs/primitives/hover-card",
-//     description:
-//       "For sighted users to preview content available behind a link.",
-//   },
-//   {
-//     title: "Progress",
-//     href: "/docs/primitives/progress",
-//     description:
-//       "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-//   },
-//   {
-//     title: "Scroll-area",
-//     href: "/docs/primitives/scroll-area",
-//     description: "Visually or semantically separates content.",
-//   },
-//   {
-//     title: "Tabs",
-//     href: "/docs/primitives/tabs",
-//     description:
-//       "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-//   },
-//   {
-//     title: "Tooltip",
-//     href: "/docs/primitives/tooltip",
-//     description:
-//       "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-//   },
-// ]
+import {} from "./ui/separator";
 
 const navbarItems = [
   {
@@ -74,62 +21,60 @@ const navbarItems = [
     title: "Experience",
     href: "/experience",
   },
-]
+];
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => {
-    setIsOpen(prev => !prev);
-  }
+    setIsOpen((prev) => !prev);
+  };
   return (
     // sticky navbar
-    <nav className="h-[8vh] md:h-[10vh] fixed top-0 left-0 right-0 w-full bg-[var(--navbar-bg)]"> 
+    <nav className="h-[8vh] md:h-[10vh] fixed top-0 left-0 right-0 w-full bg-[var(--navbar-bg)]">
       {/* <div className="h-full w-full"> */}
-        {/* flexbox for md, containing navbar items */}
-        <div className="h-full flex justify-between md:justify-center items-center w-[96%] m-auto">
-          {/* List of navbar items. Hidden on small screens. */}
-            <ul className="hidden md:flex justify-center items-center w-1/2 m-auto gap-1">
-              <div className="flex-1 text-center">LOGO</div>
-              {navbarItems.map(item => (
-                <li 
-                key={item.title} 
-                className="flex-1 px-[1%]">
-                    <DrawOutlineButton>{item.title}</DrawOutlineButton>
-                </li>
-              ))}
-            </ul>
+      {/* flexbox for md, containing navbar items */}
+      <div className="h-full flex justify-between md:justify-center items-center w-[96%] m-auto">
+        {/* List of navbar items. Hidden on small screens. */}
+        <ul className="hidden md:flex justify-center items-center w-1/2 m-auto gap-1">
+          <div className="flex-1 text-center">LOGO</div>
+          {navbarItems.map((item) => (
+            <li key={item.title} className="flex-1 px-[1%]">
+              <DrawOutlineButton>{item.title}</DrawOutlineButton>
+            </li>
+          ))}
+        </ul>
 
-            {/* Logo. Hidden on screens md and above. */}
-            <div className="md:hidden w-1/5 text-center">LOGO</div>
-            <HamburgerButton 
-            onClick={toggleOpen} 
-            isOpen={isOpen} 
-            className="md:hidden w-1/5"
-            />
-        </div>
-      
+        {/* Logo. Hidden on screens md and above. */}
+        <div className="md:hidden w-1/5 text-center">LOGO</div>
+        <HamburgerButton
+          onClick={toggleOpen}
+          isOpen={isOpen}
+          className="md:hidden w-1/5"
+        />
+      </div>
+
       {/* Navbar items for small screens. Open when `isOpen`*/}
       {isOpen && (
         // <div className="w-full">
-          <ul className="md:hidden sticky top-0 left-0 w-full h-[20vh] flex flex-col mx-auto bg-[var(--navbar-bg)]">
-            {navbarItems.map(item => (
-              <li 
-              key={item.title} 
+        <ul className="md:hidden sticky top-0 left-0 w-full h-[20vh] flex flex-col mx-auto bg-[var(--navbar-bg)]">
+          {navbarItems.map((item) => (
+            <li
+              key={item.title}
               className="flex justify-center items-center block hover:primary-color text-center flex-1 w-[95%] m-auto mb-3"
-              >
-                <button
-                className="w-full h-full focus:bg-primary-color hover:bg-primary-color hover:text-black focus:text-black focus:outline-none rounded-lg"
-                >{item.title}</button>
-              </li>
-            ))}
-          </ul>
+            >
+              <button className="w-full h-full focus:bg-primary-color hover:bg-primary-color hover:text-black focus:text-black focus:outline-none rounded-lg">
+                {item.title}
+              </button>
+            </li>
+          ))}
+        </ul>
         // </div>
       )}
       {/* </div> */}
     </nav>
-  )
-}
+  );
+};
 
 export default NavBar;
 // const NavBar = () => {
